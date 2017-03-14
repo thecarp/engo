b03b68164027d1361df4e6ec847952602b42a9a7
Stephen Carpenter <sjc@carpanet.net>
2017-03-14 10:17:16 -0700

    Fixed Proposal Test
    Test was failing in texture upload because it didn't set the headless
    atribute.

--- common/assets_test.go
+++ common/assets_test.go
@@ -18,36 +18,42 @@ p+uO2kp8BStQB3zAP4fVf479GXwy+2AhKduP1/MeWCxuJr4XayfzHhaL95gObEvzsNB+LddMfhqt
 RTQLB99IA+8+s9bWvTl40D7XffNOM2fvTIsbhYsvnByyZXKhHYRl3belWzbRq0cDhx2hb9kbs2Hv
 mAeEtZsxG+nRkprCfA27dmpPyjHOk0kuSisVmwLLO4OcalqDpn2chumUdNuZUuU0lLNhMp2fa5Cu
 77OmWl8yp1W6uQxN+zit0OiiF3wDvMbcc/bBfZwWrhKz8deQezTyh6VO+VJ7AJzqxGwintLZAAAA
-AElFTkSuQmCC `
+AElFTkSuQmCC`
 
 // TestAssetProposal 
 func TestAssetProposal(t *testing.T) {
+	engo.SetHeadless(true)
 	// Load test data into an image.Image
 	buff, err := base64.StdEncoding.DecodeString(strings.TrimSpace(PngTestB64) )
 	img, s, err := image.Decode(bytes.NewReader(buff))
+	width := img.Bounds().Max.X
+	height := img.Bounds().Max.Y
 	if err != nil {
 		t.Errorf("Got error in decode can't demo bad data", err)
 	}
 	if s != "png" {
 		t.Errorf("Data not png? Did not expect:", s)
 	}
-	if img.Bounds().Max.X != 120 {
-		t.Errorf("Data failed to load 120 cols:", img.Bounds().Max.X)
+	if width != 120 {
+		t.Errorf("Data failed to load 120 cols:", width)
 	}
-	if img.Bounds().Max.Y != 40 {
-		t.Errorf("Data failed to load 40 rows:", img.Bounds().Max.Y)
+	if height != 40 {
+		t.Errorf("Data failed to load 40 rows:", height)
 	}
 
 	if err := engo.Files.Insert("bar.png", img); err != nil {
 		t.Errorf("Data failed to insert: %v", err)
 	}
 
-	//bar, err := engo.Files.Resource("bar.png")
-	//_, err := engo.Files.Resource("bar.png")
+	bargen, err := engo.Files.Resource("bar.png")
+	bar, _ := bargen.(TextureResource)
 
-	//if err != nil {
-	//	t.Errorf("Error loading bar.png", err)
-	//}
+	if err != nil {
+		t.Errorf("Error loading bar.png", err)
+	}
 
-	//t.Logf("Got bar! %v", bar)
+	if bar.Width != float32(width) {
+		t.Errorf("Data failed to load back %v cols: %v", width, bar.Width)
+	}
+
 }
